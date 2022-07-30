3f429ac3ce85b83b13c5c6d8109195e93aaa66f6
charleseMuchmore <charlymuchmore@gmail.com>
2022-07-29 17:27:52 -0700

    created player

---
+++ car_manager.py
@@ -0,0 +1,15 @@
+from configparser import MAX_INTERPOLATION_DEPTH
+from turtle import Turtle
+import random
+COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+STARTING_MOVE_DISTANCE = 5
+MOVE_INCREMENT = 10
+MAX_PIXELS_FROM_CENTER = 250
+
+
+class CarManager(Turtle):
+    
+    def __init__(self):
+        super().__init__()
+
+
